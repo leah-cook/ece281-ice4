d5d238c349a1b7a47d842cbee2f529c0f35a894a
leah-cook <156127890+leah-cook@users.noreply.github.com>
2024-03-12 14:13:30 -0600

    initial commit

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : top_basys3.vhd
---| AUTHOR(S)     : Capt Phillip Warner
---| CREATED       : 02/22/2018 Modified: 03/01/2020 by capt Dan Johnson
+--| AUTHOR(S)     : leah Cook
+--| CREATED       : 02/22/2018 Modified: 03/12/2024 by Leah Cook
 --| DESCRIPTION   : This file implements the top level module for the solution for Stoplight FSM.
 --|
 --|					Inputs:  clk 	--> 100 MHz clock from FPGA
